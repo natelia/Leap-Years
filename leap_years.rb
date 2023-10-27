9741b1f8e2416db6ff714a9054b15a1383ef69f0
natelia <natalia.jadwiga.gajewska@gmail.com>
2023-10-27 22:04:13 +0200

    first commit

---
+++ leap_years.rb
@@ -0,0 +1,14 @@
+puts "Give me a year you would like to start with."
+starting_year = gets.chomp.to_i
+
+puts "Now, type in year of your choice, at which you want me to stop counting."
+ending_year = gets.chomp.to_i
+
+
+year = starting_year 
+while year <= ending_year
+    if (year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 )
+     puts year
+    end
+    year += 1
+end
