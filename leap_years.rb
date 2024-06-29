19e6a8cffb7207755873f97fb74c9ac5d02fbd1a
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-29 22:39:02 +0200

    Refactored code for readability and style compliance

--- leap_years.rb
+++ leap_years.rb
@@ -1,14 +1,13 @@
-puts "Give me a year you would like to start with."
+# frozen_string_literal: true
+
+puts 'Give me a year you would like to start with.'
 starting_year = gets.chomp.to_i
 
-puts "Now, type in year of your choice, at which you want me to stop counting."
+puts 'Now, type in year of your choice, at which you want me to stop counting.'
 ending_year = gets.chomp.to_i
 
-
-year = starting_year 
+year = starting_year
 while year <= ending_year
-    if (year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 )
-     puts year
-    end
-    year += 1
+  puts year if (year % 4).zero? && (year % 100 != 0) || (year % 400).zero?
+  year += 1
 end
